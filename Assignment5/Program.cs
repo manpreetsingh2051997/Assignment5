8c866be3bd2697f227daed422c251f25a9bd0563
visualstudio <visualstudio@122-PC18>
2019-04-12 17:53:30 -0400

    Assignment_5_Gematria

---
+++ Assignment5/Program.cs
@@ -0,0 +1,15 @@
+﻿5using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
